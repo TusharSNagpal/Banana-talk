052ba51163bc230730c270326577923cda626259
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2021-04-08 20:17:04 +0530

    input-processing-output

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var btnTrans = document.querySelector("#btn-trans");
 var txtArea = document.querySelector("#txt-area");
+var outputDisp = document.querySelector("#output");
 
-var userName = prompt("Hey! What is your name?");
-alert("Welcome, " +  userName + ".");
+/*var userName = prompt("Hey! What is your name?");
+alert("Welcome, " +  userName + ".");*/
 
-function whatOnClick() {  
+function whatOnClick() {
+    outputDisp.innerText = "This text is translated " + txtArea.value + ".";
+    
     console.log("Clicked!");
     console.log(txtArea.value);
 };
