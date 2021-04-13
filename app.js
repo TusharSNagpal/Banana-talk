a64aa5099c8cc5ff3176ee408926cec49a58ec6b
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2021-04-13 20:24:33 +0530

    update-server-link

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ var outputDisp = document.querySelector("#output");
 /*var userName = prompt("Hey! What is your name?");
 alert("Welcome, " +  userName + ".");*/
 
+var serverLink = "https://test.tusharnagpal.repl.co/translate/yoda.json";
+function constructURL(i)
+{
+    return serverLink + "?" + "text=" + i
+}
 function whatOnClick() {
-    outputDisp.innerText = "This text is translated " + txtArea.value + ".";
+    /*outputDisp.innerText = "This text is translated " + txtArea.value + ".";
     
     console.log("Clicked!");
-    console.log(txtArea.value);
+    console.log(txtArea.value);*/
+    var input = txtArea.value;
+    fetch(constructURL(input))
+    .then(response => response.json())
+    .then(json => console.log(json))
 };
 
 btnTrans.addEventListener("click", whatOnClick);
