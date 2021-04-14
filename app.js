4e3d79dcc85481c8969eaeb358c3633544da64af
Tushar Nagpal <63884440+TusharSNagpal@users.noreply.github.com>
2021-04-14 19:57:55 +0530

    Initial-commit

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ var outputDisp = document.querySelector("#output");
 /*var userName = prompt("Hey! What is your name?");
 alert("Welcome, " +  userName + ".");*/
 
-var serverLink = "https://test.tusharnagpal.repl.co/translate/yoda.json";
+//var serverLink = "https://test.tusharnagpal.repl.co/translate/yoda.json";
+var serverLink = "https://api.funtranslations.com/translate/ferb-latin.json";
+
 function constructURL(i)
 {
     return serverLink + "?" + "text=" + i
 }
+
+function errorHandler(error){
+    console.log("Error Occurred",error)
+}
+
 function whatOnClick() {
     /*outputDisp.innerText = "This text is translated " + txtArea.value + ".";
     
@@ -18,7 +25,8 @@ function whatOnClick() {
     var input = txtArea.value;
     fetch(constructURL(input))
     .then(response => response.json())
-    .then(json => console.log(json))
-};
+    .then(json => outputDisp.innerText=json.contents.translated)
+    .catch(errorHandler)
+}
 
-btnTrans.addEventListener("click", whatOnClick);
+btnTrans.addEventListener("click", whatOnClick)
